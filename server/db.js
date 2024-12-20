13992346bde4597d1841ff20d25cfcb543c662a9
saravanan b s <132475341+saravananbs@users.noreply.github.com>
2024-12-20 10:58:15 +0530

    Update db.js to  hide pass

--- server/db.js
+++ server/db.js
@@ -3,7 +3,7 @@ const mysql = require('mysql2');
 const pool = mysql.createPool({
     host: 'localhost',
     user: 'root',
-    password: 'mysql123',
+    password: '', //your password here
     database: 'EmployeeDB',
 });
 
